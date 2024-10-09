3a3a9a856d646fa6310c27b79a968e0131c79a72
gabogara <gabo1gara@gmail.com>
2024-10-09 11:40:53 -0700

    listen to our microphone and add function so that the assistant can be heard

---
+++ virtual_asistant.py
@@ -0,0 +1,29 @@
+import pyttsx3
+import speech_recognition as sr
+import pywhatkit
+import yfinance as yf
+import pyjokes
+import webbrowser
+import datetime
+import wikipedia
+
+# listen to our microphone and return the audio as text
+def transform_audio_to_text():
+    # store recognizer in variable
+    r = sr.Recognizer()
+    # microphone configuration
+
+
+
+# function so that the assistant can be heard
+def speak(message):
+
+    # start the engine of pyttsx3
+    engine = pyttsx3.init()
+
+
+    # deliver message
+    engine.say(message)
+    engine.runAndWait()
+
+speak("Hello world")
