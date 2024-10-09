f5e5b69ddbebc93052e41ee304ddae2f72862ee2
gabogara <gabo1gara@gmail.com>
2024-10-09 12:25:36 -0700

    add languages options

--- virtual_asistant.py
+++ virtual_asistant.py
@@ -1,11 +1,11 @@
 import pyttsx3
 import speech_recognition as sr
-import pywhatkit
-import yfinance as yf
-import pyjokes
-import webbrowser
-import datetime
-import wikipedia
+#import pywhatkit
+#import yfinance as yf
+#import pyjokes
+#import webbrowser
+#import datetime
+#import wikipedia
 
 # listen to our microphone and return the audio as text
 def transform_audio_to_text():
@@ -21,9 +21,13 @@ def speak(message):
     # start the engine of pyttsx3
     engine = pyttsx3.init()
 
-
     # deliver message
     engine.say(message)
     engine.runAndWait()
+    engine.setProperty('voice', id2)
 
-speak("Hello world")
+# voice options / language
+id1 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0'
+id2 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
+
+speak('Hello Gabriel I hope you will have a great day')
